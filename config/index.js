bb6c5a3d753b1b18c1b3b1ff8e25ee9e6420bb97
罗鹏飞 <luopf@cn.ibm.com>
2021-09-10 20:30:33 +0800

    打包 +  注释

--- config/index.js
+++ config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../bennyluo.cf:666/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../bennyluo.cf:666'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
--- src/pages/taxiTravel.vue
+++ src/pages/taxiTravel.vue
@@ -1,54 +1,67 @@
 <template>
-  <div class="taxiTravel">
+  <div
+    class="taxiTravel"
+    :class="isMobile ? 'top' : ''"
+  >
     <el-form
-      :inline="true"
       :model="formInline"
       class="demo-form-inline"
     >
       <el-row :gutter="30">
-        <el-col :span="6">
-          <el-form-item label="时开始">
+        <el-col
+          :xs="12"
+          :md="6"
+        >
+          <el-form-item label="小时开始">
             <el-input-number
               size="small"
               clearable
               v-model="formInline.hoursStart"
-              placeholder="时开始"
             ></el-input-number>
           </el-form-item>
         </el-col>
-        <el-col :span="6">
-          <el-form-item label="时结束">
+        <el-col
+          :xs="12"
+          :md="6"
+        >
+          <el-form-item label="小时结束">
             <el-input-number
               size="small"
               clearable
               v-model="formInline.hoursEnd"
-              placeholder="时结束"
             ></el-input-number>
           </el-form-item>
         </el-col>
-        <el-col :span="6">
-          <el-form-item label="分开始">
+        <el-col
+          :xs="12"
+          :md="6"
+        >
+          <el-form-item label="分钟开始">
             <el-input-number
               size="small"
               clearable
               v-model="formInline.minutesStart"
-              placeholder="分开时"
             ></el-input-number>
           </el-form-item>
         </el-col>
-        <el-col :span="6">
-          <el-form-item label="分结束">
+        <el-col
+          :xs="12"
+          :md="6"
+        >
+          <el-form-item label="分钟结束">
             <el-input-number
               size="small"
               clearable
               v-model="formInline.minutesEnd"
-              placeholder="分结束"
             ></el-input-number>
           </el-form-item>
         </el-col>
       </el-row>
       <el-row :gutter="20">
-        <el-col :span="6">
+        <el-col
+          :xs="12"
+          :md="6"
+        >
           <el-form-item label="公里开始">
             <el-input-number
               size="small"
@@ -58,7 +71,10 @@
             ></el-input-number>
           </el-form-item>
         </el-col>
-        <el-col :span="6">
+        <el-col
+          :xs="12"
+          :md="6"
+        >
           <el-form-item label="公里结束">
             <el-input-number
               size="small"
@@ -68,8 +84,11 @@
             ></el-input-number>
           </el-form-item>
         </el-col>
-        <el-col :span="12">
-          <el-form-item label="日期范围">
+        <el-col :md="12">
+          <el-form-item
+            label="日期范围"
+            v-show="!isMobile"
+          >
             <el-date-picker
               v-model="formInline.value"
               type="daterange"
@@ -81,21 +100,22 @@
               clearable
               align="center"
               style="margin-bottom:20px"
-              v-show="!isMobile"
               size="small"
             >
             </el-date-picker>
-            <inlineCalendar
-              ref="myCalendar"
-              v-show="isMobile"
-              mode="during"
-              style="margin-bottom:20px"
-              @change="onChange"
-              enableTouch
-              class="mobileDate"
-            />
           </el-form-item>
         </el-col>
+      </el-row>
+      <el-row>
+        <inlineCalendar
+          ref="myCalendar"
+          v-show="isMobile"
+          mode="during"
+          style="margin-bottom:20px"
+          @change="onChange"
+          enableTouch
+          class="mobileDate"
+        />
       </el-row>
       <el-form-item>
         <el-button
@@ -114,17 +134,17 @@ export default {
   data () {
     return {
       formInline: {
-        value: [],
-        rangeValue: [],
-        randomHours: "",
-        randomMinutes: "",
-        randomKm: "",
-        hoursStart: 21,
-        hoursEnd: 21,
-        minutesStart: 0,
-        minutesEnd: 59,
-        kmStart: 34,
-        kmEnd: 35
+        value: [], // 选中日期范围的开始日期时间戳和结束日期时间戳
+        rangeValue: [], // 生成合并后的数据，例如：["2021-09-10 21:13 34.4Km","2021-09-11 21:04 34.9Km"]
+        randomHours: "", // 生成的小时数据
+        randomMinutes: "", // 生成的分钟数据
+        randomKm: "", // 生成的公里数据
+        hoursStart: 21, // 小时开始
+        hoursEnd: 21, // 小时结束
+        minutesStart: 0, // 分钟开始
+        minutesEnd: 59, // 分钟结束
+        kmStart: 34, // 公里开始
+        kmEnd: 35 // 公里结束
       }
     };
   },
@@ -146,31 +166,45 @@ export default {
         });
         return;
       }
+      // 得到选中日期范围的开始日期
       let start = this.formInline.value[0] - 1000 * 3600 * 24;
+      // 得到选中日期范围的结束日期
       let end = this.formInline.value[1];
+      // 获取一共有多少天
       let length = (end - start) / (1000 * 3600 * 24);
+      // 循环处理整合数据，逐一push到rangeValue数组中
       for (let index = 0; index < length; index++) {
         let startDate = moment(start).add(1, "days").format("YYYY-MM-DD");
+        // 生成随机小时
         this.formInline.randomHours = this.randomNum(this.formInline.hoursStart, this.formInline.hoursEnd, 0);
+        // 生成随机分钟
         this.formInline.randomMinutes = this.randomNum(this.formInline.minutesStart, this.formInline.minutesEnd, 0);
+        // 生成随机公里数
         this.formInline.randomKm = this.toInteger(this.randomNum(this.formInline.kmStart, this.formInline.kmEnd, 1))
+        // 判断如果生成的随机小时等于输入框输入的小时结束时间，则分钟数除以4 （目的是生成的分钟数不那么假）
         if (this.formInline.randomHours === this.formInline.hoursEnd) {
           this.formInline.randomMinutes = (this.formInline.randomMinutes / 4).toFixed();
         }
+        // 分钟数补0
         if (this.formInline.randomMinutes < 10) {
           this.formInline.randomMinutes = "0" + this.formInline.randomMinutes;
         }
+        // 数据组合
         let startDateTime = `${startDate} ${this.formInline.randomHours}:${this.formInline.randomMinutes} ${this.formInline.randomKm}Km`;
+        // 最终逐一 push 到 rangeValue 数组中
         this.formInline.rangeValue.push(startDateTime);
         start = startDate;
         this.formInline.randomHours = "";
         this.formInline.randomMinutes = "";
       }
+      // 执行数据的 copy 功能
       this.onCopy()
     },
+    // 生成随机数函数
     randomNum (minNum, maxNum, fixedTo) {
       return (Math.random() * (maxNum - minNum) + minNum).toFixed(fixedTo) * 1;
     },
+    // 判断是否是整数，若是整数，后面加 .0 展示，例如：35 => 35.0
     toInteger (obj) {
       if (typeof obj === 'number' && obj % 1 === 0) {
         return obj + '.0'
@@ -178,6 +212,7 @@ export default {
         return obj
       }
     },
+    // 复制功能
     onCopy () {
       this.$copyText(this.copyRangeValue).then(() => {
         this.$notify({
@@ -195,10 +230,12 @@ export default {
     },
   },
   computed: {
+    // 复制最终生成的数据
     copyRangeValue () {
       const reg = new RegExp(",", "g");
       return this.formInline.rangeValue.toString().replace(reg, "\n");
     },
+    // 判断是否是手机端
     isMobile () {
       let flag = /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i.test(
         navigator.userAgent
@@ -227,7 +264,10 @@ export default {
   transform: translate(-50%, -40%);
 }
 .setTime {
-  margin-bottom: 80px;
+  margin-bottom: 60px;
+}
+.top {
+  margin-top: 20px;
 }
 .mobileDate {
   width: 150%;
